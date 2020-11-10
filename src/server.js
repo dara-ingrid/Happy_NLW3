8681dc714d4f28768181c2b776dbd8c99b1f34b8
Dara <dara-ingrid@hotmail.com>
2020-11-10 15:09:06 -0300

    ✨Join Front-end and  Back-end

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
 //importar dependencia
-const express = require('express')
+const express = require("express");
+const path = require("path");
+const pages = require('./pages.js')
 
 //Iniciando o express
-const server  = express()
+const server = express();
+server
+    //utilizando arquivos estáticos
+    .use(express.static("public"))
 
-//criar a rota
-server.get('/', (request, response) => {
-  return response.send('Oi! Direto do Back-end')
-})
+    //configurar template engine
+    .set('views', path.join(__dirname, "views"))
+    .set('view engine', 'hbs')
+
+    //rotas da aplicação
+    .get("/", pages.index)
+    .get("/orphanage", pages.orphanage)
+    .get("/orphanages", pages.orphanages)
+    .get("/create-orphanage", pages.createOrphanage)
 
 //ligar o servidor
-server.listen(5500)
+server.listen(5500);
